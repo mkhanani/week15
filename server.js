ea072b6bf7a23a1ff86df43e395bffef62060439
Mustafa <mustafa_khanani@outlook.com>
2024-02-02 15:29:23 +0400

    Update server.js

--- server.js
+++ server.js
@@ -77,6 +77,8 @@ app.delete('/collection/:collectionName/:id', (req, res, next) => {
         })
 })
 
-app.listen(3000, () =>{
-    console.log('Express.js server is running on localhost:3000')
-})
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Port 3000");
+});
